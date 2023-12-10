96b26317d617e2363bdce240320f36f07d295052
root <sqsiek0>
2023-12-10 13:38:52 +0100

    changes to return

--- serwer.py
+++ serwer.py
@@ -27,15 +27,15 @@ def walking():
         if walk == 'front':
             variables.robotState = '4'
             variables.moveDirection = '1'
-            return 200
+            return jsonify({'Correct'}, 200)
         elif walk == 'back':
             variables.robotState = '4'
             variables.moveDirection = '2'
-            return 200
+            return jsonify({'Correct'}, 200)
         elif walk == 'stop':
             variables.robotState = '1'
             variables.moveDirection = '1'
-            return 200
+            return jsonify({'Correct'}, 200)
     except:
         return jsonify({'Error during walking', 400})
     
@@ -50,10 +50,10 @@ def turning():
         
         if turn == 'left':
             variables.robotState = '5'
-            return 200
+            return jsonify({'Correct'}, 200)
         elif turn == 'right':
             variables.robotState = '6'
-            return 200
+            return jsonify({'Correct'}, 200)
     except:
         return jsonify({'Error during turning', 400})
     
@@ -70,15 +70,15 @@ def translation():
         if axis == 'x':
             if value == 'plus': variables.translationState = 'w'
             if value == 'minus': variables.translationState = 's'
-            return 200
+            return jsonify({'Correct'}, 200)
         elif axis == 'y':
             if value == 'plus': variables.translationState = 'd'
             if value == 'minus': variables.translationState = 'a'
-            return 200
+            return jsonify({'Correct'}, 200)
         elif axis == 'z':
             if value == 'plus': variables.translationState = 'q'
             if value == 'minus': variables.translationState = 'e'
-            return 200
+            return jsonify({'Correct'}, 200)
     except:
         return jsonify({'Error during translation', 400})
     
@@ -95,14 +95,14 @@ def rotate():
         if axis == 'x':
             if value == 'plus': variables.rotationState = 'u'
             if value == 'minus': variables.rotationState = 'i'
-            return 200
+            return jsonify({'Correct'}, 200)
         elif axis == 'y':
             if value == 'plus': variables.rotationState = 'j'
             if value == 'minus': variables.rotationState = 'k'
-            return 200
+            return jsonify({'Correct'}, 200)
         elif axis == 'z':
             if value == 'plus': variables.rotationState = 'm'
             if value == 'minus': variables.rotationState = ','
-            return 200
+            return jsonify({'Correct'}, 200)
     except:
         return jsonify({'Error during translation', 400})
