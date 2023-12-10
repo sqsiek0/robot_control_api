7146c1a92bf766c6b344f95322c4b00bfcd0d63e
root <sqsiek0>
2023-12-10 13:32:14 +0100

    basic api logic

--- serwer.py
+++ serwer.py
@@ -3,16 +3,6 @@ import variables
 
 app = Flask(__name__)
 
-
-@app.route('/control', methods=['POST'])
-def control():
-    data = request.get_json()
-    if data is None:
-        
-        return "Invalid JSON data", 400
-    
-    return {'success': True}
-
 @app.route('/working', methods = ['GET'])
 def working():
     try:    
@@ -24,3 +14,95 @@ def working():
             return jsonify({'isWorking': variables.isRobotWork}), 200
     except:
         return jsonify({'Error during checking robot', 400})
+    
+@app.route('/walking', methods = ['POST'])
+def walking():
+    try:
+        data = request.get_json()
+        if data is None:
+            return jsonify({'No body provided', 400})
+        
+        walk = data['walkState']
+        
+        if walk == 'front':
+            variables.robotState = '4'
+            variables.moveDirection = '1'
+            return 200
+        elif walk == 'back':
+            variables.robotState = '4'
+            variables.moveDirection = '2'
+            return 200
+        elif walk == 'stop':
+            variables.robotState = '1'
+            variables.moveDirection = '1'
+            return 200
+    except:
+        return jsonify({'Error during walking', 400})
+    
+@app.route('/turning', methods = ['POST'])
+def turning():
+    try:
+        data = request.get_json()
+        if data is None:
+            return jsonify({'No body provided', 400})
+        
+        turn = data['turn']
+        
+        if turn == 'left':
+            variables.robotState = '5'
+            return 200
+        elif turn == 'right':
+            variables.robotState = '6'
+            return 200
+    except:
+        return jsonify({'Error during turning', 400})
+    
+@app.route('/translation', methods = ['post'])
+def translation():
+    try:
+        data = request.get_json()
+        if data is None:
+            return jsonify({'No body provided', 400})
+        
+        axis = data['axis']
+        value = data['value']
+        
+        if axis == 'x':
+            if value == 'plus': variables.translationState = 'w'
+            if value == 'minus': variables.translationState = 's'
+            return 200
+        elif axis == 'y':
+            if value == 'plus': variables.translationState = 'd'
+            if value == 'minus': variables.translationState = 'a'
+            return 200
+        elif axis == 'z':
+            if value == 'plus': variables.translationState = 'q'
+            if value == 'minus': variables.translationState = 'e'
+            return 200
+    except:
+        return jsonify({'Error during translation', 400})
+    
+@app.route('/rotate', methods = ['post'])
+def rotate():
+    try:
+        data = request.get_json()
+        if data is None:
+            return jsonify({'No body provided', 400})
+        
+        axis = data['axis']
+        value = data['value']
+        
+        if axis == 'x':
+            if value == 'plus': variables.rotationState = 'u'
+            if value == 'minus': variables.rotationState = 'i'
+            return 200
+        elif axis == 'y':
+            if value == 'plus': variables.rotationState = 'j'
+            if value == 'minus': variables.rotationState = 'k'
+            return 200
+        elif axis == 'z':
+            if value == 'plus': variables.rotationState = 'm'
+            if value == 'minus': variables.rotationState = ','
+            return 200
+    except:
+        return jsonify({'Error during translation', 400})
--- variables.py
+++ variables.py
@@ -1,1 +1,39 @@
 isRobotWork = True
+robotState = '0'
+moveDirection = '1'
+translationState = ''
+rotationState = ''
+
+# Stany robota
+chooseRobotState = {
+    '0': "idle",
+    '4': "walk",
+    '5': "rotate_left",
+    '6': "rotate_right",
+    '7': "body_manipulation",
+    '8': "torque_enable",
+    '9': "torque_disable",
+}
+
+movementDirection = {
+    '1': 1,
+    '2': -1,
+}
+
+moveBindings = {
+    'w': (10, 0, 0),
+    's': (-10, 0, 0),
+    'd': (0, 10, 0),
+    'a': (0, -10, 0),
+    'q': (0, 0, 10),
+    'e': (0, 0, -10),
+}
+
+rotationBindings = {
+    'u': (2, 0, 0),
+    'i': (-2, 0, 0),
+    'j': (0, 2, 0),
+    'k': (0, -2, 0),
+    'm': (0, 0, 2),
+    ',': (0, 0, -2),
+}
