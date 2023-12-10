b87002db1e5caeaf812b604a99978c31d414141e
root <sqsiek0>
2023-12-10 12:27:01 +0100

    working method

--- serwer.py
+++ serwer.py
@@ -1,11 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+isRobotWork = True
 
 @app.route('/control', methods=['POST'])
 def control():
     data = request.get_json()
     if data is None:
+        
         return "Invalid JSON data", 400
     
     return {'success': True}
+
+@app.route('/working', methods = ['GET'])
+def working():
+    try:    
+        if isRobotWork:
+            isRobotWork = False
+            return jsonify({'isWorking': isRobotWork}), 200
+        else:
+            isRobotWork = True;
+            return jsonify({'isWorking': isRobotWork}), 200
+    except:
+        return jsonify({'Error during checking robot', 400})
