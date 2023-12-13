1d19c081c210b34a8047b0a731dec945dc9e99a6
root <sqsiek0>
2023-12-13 20:41:21 +0100

    trying the port 8080

--- serwer.py
+++ serwer.py
@@ -169,4 +169,4 @@ def rotate():
         return jsonify({'error': 'Error during rotation', 'details': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
